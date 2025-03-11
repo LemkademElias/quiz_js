57aff995cc2b59300c6f087a60f3a0da1a964cbc
elias-git <lemkademelias@gmail.com>
2025-03-11 17:25:37 +0100

    tableau de questions

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 let score = 0;
 let question;
 let reponse;
+
+const questions = [
+    "Quel joueur a remporté le plus de Ballons d'Or ?",
+    "Quelle équipe a gagné la Coupe du Monde 2018 ?",
+    "Quel est le surnom de Cristiano Ronaldo ?",
+    "Dans quel club joue Lionel Messi en 2024 ?",
+    "Quel pays a organisé la Coupe du Monde 2014 ?",
+    "Quel joueur est surnommé 'La Pulga' ?",
+    "Combien de joueurs sur le terrain dans une équipe de football ?",
+    "Quel est le nom du stade du Real Madrid ?",
+    "Quel numéro portait Zinédine Zidane en équipe de France ?",
+    "Quel club est surnommé 'Les Reds' ?"
+  ];
+
+  
